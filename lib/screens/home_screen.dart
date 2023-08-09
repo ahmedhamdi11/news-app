39740ad375437834f92480118145471705036079
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-08-09 19:36:15 +0300

    search screen created

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iconly/iconly.dart';
+import 'package:news_app/screens/search_screen.dart';
+import 'package:news_app/utils/cusom_page_route_tranition.dart';
 import 'package:news_app/widgets/drawer_widgets/custom_drawer.dart';
 import 'package:news_app/widgets/home_widgets/home_screen_body.dart';
 
@@ -17,7 +19,12 @@ class HomeScreen extends StatelessWidget {
         ),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () => Navigator.of(context).push(
+              CustomPageRouteTransition(
+                page: const SearchScreen(),
+                transitionType: TransitionTypeEnum.fade,
+              ),
+            ),
             icon: const Icon(IconlyBroken.search),
           )
         ],
