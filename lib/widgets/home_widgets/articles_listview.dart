c5599a2278a7bf0d2ecf41fc5f0c63c139c3da50
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-08-10 13:57:41 +0300

    staggered animations added

--- lib/widgets/home_widgets/articles_listview.dart
+++ lib/widgets/home_widgets/articles_listview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:news_app/widgets/home_widgets/articles_item.dart';
 
 class ArticlesListView extends StatelessWidget {
@@ -6,9 +7,26 @@ class ArticlesListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 20,
-      itemBuilder: (context, index) => const ArticleItme(),
+    return AnimationLimiter(
+      child: ListView.builder(
+        itemCount: 20,
+        itemBuilder: (context, index) => AnimationConfiguration.staggeredList(
+          position: index,
+          delay: const Duration(milliseconds: 100),
+          child: const SlideAnimation(
+            duration: Duration(milliseconds: 1500),
+            curve: Curves.fastLinearToSlowEaseIn,
+            horizontalOffset: 40,
+            verticalOffset: 150.0,
+            child: FlipAnimation(
+              duration: Duration(milliseconds: 2500),
+              curve: Curves.fastLinearToSlowEaseIn,
+              flipAxis: FlipAxis.y,
+              child: ArticleItme(),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/widgets/home_widgets/top_trending_view.dart
+++ lib/widgets/home_widgets/top_trending_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:news_app/widgets/home_widgets/top_trending_article_item.dart';
 import 'package:card_swiper/card_swiper.dart';
 
@@ -10,13 +11,24 @@ class TopTrendingView extends StatelessWidget {
     double w = MediaQuery.of(context).size.width;
     double h = MediaQuery.of(context).size.height;
 
-    return SizedBox(
-      height: h * 0.6,
-      child: Swiper(
-        itemWidth: w * 0.9,
-        layout: SwiperLayout.STACK,
-        itemCount: 5,
-        itemBuilder: (context, index) => const TopTrendingArticleItem(),
+    return AnimationConfiguration.synchronized(
+      child: SlideAnimation(
+        curve: Curves.fastLinearToSlowEaseIn,
+        duration: const Duration(milliseconds: 1500),
+        horizontalOffset: 60,
+        verticalOffset: 80,
+        child: FadeInAnimation(
+          duration: const Duration(milliseconds: 1500),
+          child: SizedBox(
+            height: h * 0.55,
+            child: Swiper(
+                itemWidth: w * 0.9,
+                layout: SwiperLayout.STACK,
+                itemCount: 5,
+                itemBuilder: (context, index) =>
+                    const TopTrendingArticleItem()),
+          ),
+        ),
       ),
     );
   }
