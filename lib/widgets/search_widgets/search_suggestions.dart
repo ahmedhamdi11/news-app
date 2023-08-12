0589b5cabde7e38c1435d6545a754aa379e1a755
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-08-12 23:37:21 +0300

    adding animation to the search suggestions

--- lib/widgets/search_widgets/search_suggestions.dart
+++ lib/widgets/search_widgets/search_suggestions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:news_app/constents/constants.dart';
 import 'package:news_app/widgets/search_widgets/search_suggestion_item.dart';
 
@@ -7,22 +8,34 @@ class SearchSuggestions extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Wrap(
-          spacing: 8.0,
-          runSpacing: 8.0,
-          children: List.generate(
-            kSearchKeywords.length, // replace with your dynamic data length
-            (index) {
-              // replace with your dynamic data
-              return SearchSuggestionItem(
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Wrap(
+        spacing: 8.0,
+        runSpacing: 8.0,
+        children: List.generate(
+          kSearchKeywords.length,
+          (index) {
+            return Animate(
+              effects: [
+                SlideEffect(
+                  delay: Duration(milliseconds: 80 * index),
+                  begin: const Offset(1, 0),
+                  end: const Offset(0, 0),
+                  curve: Curves.fastLinearToSlowEaseIn,
+                  duration: const Duration(milliseconds: 1500),
+                ),
+                const FadeEffect(
+                  curve: Curves.fastLinearToSlowEaseIn,
+                  duration: Duration(seconds: 4),
+                ),
+              ],
+              child: SearchSuggestionItem(
                 text: kSearchKeywords[index],
                 onTap: () {},
-              );
-            },
-          ),
+              ),
+            );
+          },
         ),
       ),
     );
